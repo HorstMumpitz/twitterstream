15496adb0ca40dd2295e53924a5d0ee38810092f
Horst Mumpitz <denniskluge@me.com>
2014-07-18 12:21:22 +0200

    added Bson Tags

--- place.go
+++ place.go
@@ -1,30 +1,30 @@
 package twitterstream
 
 type Place struct {
-    // Contains a hash of variant information about the place.
-    Attributes map[string]interface{} `json:"attributes"`
+	// Contains a hash of variant information about the place.
+	Attributes map[string]interface{} `json:"attributes"`
 
-    // A bounding box of coordinates which encloses this place.
-    BoundingBox Box `json:"bounding_box"`
+	// A bounding box of coordinates which encloses this place.
+	BoundingBox Box `json:"bounding_box" bson:"bounding_box"`
 
-    // Name of the country containing this place.
-    Country string `json:"country"`
+	// Name of the country containing this place.
+	Country string `json:"country"`
 
-    // Shortened country code representing the country containing this place.
-    CountryCode string `json:"country_code"`
+	// Shortened country code representing the country containing this place.
+	CountryCode string `json:"country_code" bson:"country_code"`
 
-    // Full human-readable representation of the place's name.
-    FullName string `json:"full_name"`
+	// Full human-readable representation of the place's name.
+	FullName string `json:"full_name" bson:"full_name"`
 
-    // ID representing this place. Note that this is represented as a string, not an integer.
-    Id  string `json:"id"`
+	// ID representing this place. Note that this is represented as a string, not an integer.
+	Id string `json:"id"`
 
-    // Short human-readable representation of the place's name.
-    Name string `json:"name"`
+	// Short human-readable representation of the place's name.
+	Name string `json:"name"`
 
-    // The type of location represented by this place.
-    Type string `json:"place_type"`
+	// The type of location represented by this place.
+	Type string `json:"place_type" bson:"place_type"`
 
-    // URL representing the location of additional place metadata for this place.
-    Url string `json:"url"`
+	// URL representing the location of additional place metadata for this place.
+	Url string `json:"url"`
 }
