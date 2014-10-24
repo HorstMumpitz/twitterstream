f33a04cedfc6ccca23e6cddfc14ed9ca637afc20
Horst Mumpitz <denniskluge@me.com>
2014-10-24 12:21:29 +0200

    added capability to usa a proxy while tracking a stream of keywords

--- client.go
+++ client.go
@@ -43,17 +43,22 @@ func NewClientTimeout(consumerKey, consumerSecret, accessToken, accessSecret str
 
 func (c *Client) Track(keywords ...string) (*Connection, error) {
 	form := url.Values{"track": {strings.Join(keywords, ",")}}
-	return c.filter(form)
+	return c.filter(form, "")
+}
+
+func (c *Client) TrackThroughProxy(proxyURL string, keywords ...string) (*Connection, error) {
+	form := url.Values{"track": {strings.Join(keywords, ",")}}
+	return c.filter(form, proxyURL)
 }
 
 func (c *Client) Follow(userIds ...string) (*Connection, error) {
 	form := url.Values{"follow": {strings.Join(userIds, ",")}}
-	return c.filter(form)
+	return c.filter(form, "")
 }
 
 func (c *Client) Locations(p1 Point, p2 Point) (*Connection, error) {
 	form := url.Values{"locations": {fmt.Sprintf("%f,%f,%f,%f", p1.Long, p1.Lat, p2.Long, p2.Lat)}}
-	return c.filter(form)
+	return c.filter(form, "")
 }
 
 func (c *Client) Sample() (*Connection, error) {
@@ -64,7 +69,7 @@ func (c *Client) Sample() (*Connection, error) {
 
 	req.Header.Set("Authorization", c.Oauth.AuthorizationHeader(c.Credentials, "GET", req.URL, nil))
 
-	conn := newConnection(c.Timeout)
+	conn := newConnection(c.Timeout, "")
 	resp, err := conn.client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("twitterstream: making sample request failed: %s", err)
@@ -81,7 +86,7 @@ func (c *Client) Sample() (*Connection, error) {
 	return conn, nil
 }
 
-func (c *Client) filter(form url.Values) (*Connection, error) {
+func (c *Client) filter(form url.Values, proxyURL string) (*Connection, error) {
 	req, err := http.NewRequest("POST", FilterUrl, strings.NewReader(form.Encode()))
 	if err != nil {
 		return nil, fmt.Errorf("twitterstream: creating filter request failed: %s", err)
@@ -90,7 +95,8 @@ func (c *Client) filter(form url.Values) (*Connection, error) {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Authorization", c.Oauth.AuthorizationHeader(c.Credentials, "POST", req.URL, form))
 
-	conn := newConnection(c.Timeout)
+	conn := c.buildConnection(proxyURL)
+
 	resp, err := conn.client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("twitterstream: making filter request failed: %s", err)
@@ -105,4 +111,12 @@ func (c *Client) filter(form url.Values) (*Connection, error) {
 	conn.setup(resp.Body)
 
 	return conn, nil
+}
+
+func (c *Client) buildConnection(proxyURL string) *Connection {
+	if proxyURL == "" {
+		return newConnection(c.Timeout, "")
+	} else {
+		return newConnection(c.Timeout, proxyURL)
+	}
 }
